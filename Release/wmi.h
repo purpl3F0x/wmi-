55cdc5dd5514ae7e523fac8f0b1c49a797b585fd
purpl3F0x <stavros9899@gmail.com>
2019-02-10 10:40:45 +0200

    changed Release Structure
    Signed-off-by: Stavros Avramidis <stavros9899@gmail.com>

---
+++ Release/wmi.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+#include <regex>
+
+#include <comdef.h>
+#include <Wbemidl.h>
+#include <Windows.h>
+
+#pragma comment(lib, "wbemuuid.lib")
+
+typedef std::map<std::string, std::string> QueryObj;
+typedef std::map<std::string, std::regex> AdditionalFilters;
+typedef std::pair<std::string, std::regex> AdditionalFilter;
+
+class Wmi {
+/*Wmi Base class*/
+ public:
+  Wmi();
+  ~Wmi();
+
+  HRESULT init();   // Initialises connection to WMI host
+  HRESULT query(std::string queryStr,
+                std::vector<QueryObj>& queryVectorOut,
+                const AdditionalFilters* filters = nullptr);
+
+ private:
+  IWbemLocator* pLoc = nullptr;
+  IWbemServices* pSvc = nullptr;
+};
